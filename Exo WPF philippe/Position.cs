21097eaf55113ecf70f79ed2a0edc35399845d60
Romain85340 <64895507+Romain85340@users.noreply.github.com>
2020-10-20 10:50:39 +0200

    First commit

---
+++ Exo WPF philippe/Position.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.RightsManagement;
+using System.Text;
+
+namespace Exo_WPF_philippe
+{
+    public class Position
+    {
+        public int x;
+        public int y;
+
+        public Position(int _x, int _y)
+        {
+            this.x = _x;
+            this.y = _y;
+        }
+    }
+}
